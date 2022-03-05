b1a3aba567d8b29d642a2b4d4ef7bfe763f4ef0b
bochkov <bochkov-vi@yandex.ru>
2022-03-05 22:49:58 +1200

    demo spring-boot + rest + jwt + swagger

--- src/main/java/com/bochkov/OpenApi30Config.java
+++ src/main/java/com/bochkov/OpenApi30Config.java
@@ -6,8 +6,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1",
+        description = "user:admin,password:1234, пользователи user-0...user=20,password:1234"
+))
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
